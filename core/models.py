1a496a2fa859dac9f5f11fc68d4b299e0957c9b0
Azatbekov2812 <rako_004@mail.ru>
2021-09-10 19:20:34 +0600

    fix: finish

--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ class RestaurantCategory(models.Model):
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255, blank=True, null=True)
-    avatar = models.ImageField(max_length=255, blank=True, null=True)
+    avatar = models.ImageField(upload_to='pictures/restaurants/', max_length=255, blank=True, null=True)
     restaurant_category = models.ManyToManyField(RestaurantCategory, blank=True, null=True)
     food_category = models.ManyToManyField('FoodCategory', blank=True, null=True, related_name='restaurants')
 
@@ -32,7 +32,7 @@ class Food(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='restaurant_foods')
     food_category = models.ForeignKey(FoodCategory, on_delete=models.CASCADE, related_name='category_foods')
     name = models.CharField(max_length=255)
-    avatar = models.ImageField(max_length=255, blank=True, null=True)
+    avatar = models.ImageField(upload_to='pictures/foods/', max_length=255, blank=True, null=True)
 
     def __str__(self):
         return str(self.food_category) + ' -- ' + self.name
