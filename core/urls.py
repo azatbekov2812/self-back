45450dd16a1440fbf7b118a4daa51eb6a0745d4e
Azatbekov2812 <rako_004@mail.ru>
2021-09-10 12:07:29 +0600

    fix

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('restaurants/', RestaurantViews.as_view(), name='restaurants'),  # all restaurants with params 'category_id'
     path('restaurants/<int:restaurant_id>/food-categories/', FoodCategoryViews.as_view(), name='food_categories'),
     # all food category one restaurants
-    path('restaurants/<int:restaurant_id>/food-categories/foods', FoodViews.as_view(),
+    path('restaurants/<int:restaurant_id>/food-categories/foods/', FoodViews.as_view(),
          name='restaurants'),  # all foods one category
     path('restaurants/<int:restaurant_id>/food-categories/<int:food_category_id>/', FoodViews.as_view(),
          name='foods_one_categories'),  # all foods one category
