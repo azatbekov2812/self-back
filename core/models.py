ab8cce17c46b4ca98a7c1b963d5c8609bd5ca24a
Azatbekov2812 <rako_004@mail.ru>
2021-09-10 18:10:43 +0600

    fix: model

--- core/models.py
+++ core/models.py
@@ -12,9 +12,9 @@ class RestaurantCategory(models.Model):
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
     avatar = models.ImageField(max_length=255, blank=True, null=True)
-    category = models.ManyToManyField(RestaurantCategory)
+    category = models.ManyToManyField(RestaurantCategory, blank=True, null=True)
 
     def __str__(self):
         return self.name
