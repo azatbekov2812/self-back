1125f6be8d8e6c3fdc178fe5253d785ba700c8b4
Azatbekov2812 <rako_004@mail.ru>
2021-09-13 14:24:59 +0600

    add order

--- order/urls.py
+++ order/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('order/', OrderView.as_view())
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
+class OrderView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
