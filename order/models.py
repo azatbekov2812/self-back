11abbe6672e03847cbc34385320767561891ddd7
Azatbekov2812 <rako_004@mail.ru>
2021-09-13 13:51:39 +0600

    add order model

--- order/models.py
+++ order/models.py
@@ -13,4 +13,13 @@ class Reservation(models.Model):
 
 class Order(models.Model):
     creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    pass
+    streetName = models.CharField(max_length=255, default='', blank=True)
+    streetHouse = models.CharField(max_length=255, default='', blank=True)
+    apartmentOrOffice = models.CharField(max_length=255, default='', blank=True)
+    entrance = models.CharField(max_length=255, default='', blank=True)
+    floor = models.CharField(max_length=255, default='', blank=True)
+    addressComment = models.CharField(max_length=255, default='', blank=True)
+    deliveryTime = models.CharField(max_length=255, default='', blank=True)
+    payment_type = models.CharField(max_length=255, default='', blank=True)
+    peopleCount = models.CharField(max_length=255, default='', blank=True)
+    orderComment = models.CharField(max_length=255, default='', blank=True)
